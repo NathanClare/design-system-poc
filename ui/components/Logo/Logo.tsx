acac90da50ec458a6fa488fac8d416d5f5d8513c
Fabrice Werger <fabricewerger@Fabrices-MBP.youweagency.com>
2023-03-02 13:38:32 +0100

    Logo changes

--- ui/components/Logo/Logo.tsx
+++ ui/components/Logo/Logo.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import React from 'react'
 
 import CMSNLTEXT from '../../../assets/svgs/cmsnl-letters.svg'
@@ -6,37 +7,35 @@ import CMSNLLOGO from '../../../assets/svgs/cmsnl-logo.svg'
 type SVGComponent = React.FunctionComponent<React.SVGProps<SVGSVGElement>>
 
 interface LogoProps {
-    size?: 'lg' | 'md' | 'sm'
-    variant: 'standard' | 'text'
+  size?: 'lg' | 'md' | 'sm'
+  variant: 'standard' | 'text'
 }
 
 interface ILogoFamilyClasses {
-    size: Record<string, string>
-    variant: Record<string, SVGComponent>
+  size: Record<string, string>
+  variant: Record<string, SVGComponent>
 }
 
 const logoFamilyClasses: ILogoFamilyClasses = {
-    size: {
-        sm: 'h-[50px] w-[50px]',
-        md: 'h-[60px] w-[60px]',
-        lg: 'h-[100px] w-[100px]',
-    },
-    variant: {
-        standard: CMSNLLOGO,
-        text: CMSNLTEXT,
-    },
+  size: {
+    sm: 'h-[50px] w-[50px]',
+    md: 'h-[60px] w-[60px]',
+    lg: 'h-[100px] w-[100px]'
+  },
+  variant: {
+    standard: CMSNLLOGO,
+    text: CMSNLTEXT
+  }
 }
 
 const Logo = ({ size = 'md', variant = 'standard' }: LogoProps) => {
-    const SVGLogo = logoFamilyClasses['variant'][variant]
+  const SVGLogo = logoFamilyClasses['variant'][variant]
 
-    return (
-        <div
-            className={`${logoFamilyClasses['size'][size]} [&>svg]:h-full [&>svg]:w-full`}
-        >
-            <SVGLogo />
-        </div>
-    )
+  return (
+    <div className={`${logoFamilyClasses['size'][size]} [&>svg]:h-full [&>svg]:w-full`}>
+      <SVGLogo />
+    </div>
+  )
 }
 
 export default Logo
