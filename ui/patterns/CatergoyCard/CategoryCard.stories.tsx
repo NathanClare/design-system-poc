7c4f31f1becc653c20bb7900e80616c7bf2cb88d
Fabrice Werger <f.werger@youweagency.com>
2023-03-17 10:39:47 +0100

    wip

---
+++ ui/patterns/CatergoyCard/CategoryCard.stories.tsx
@@ -0,0 +1,70 @@
+import { BeakerIcon } from '@heroicons/react/24/outline'
+import type { ComponentMeta, ComponentStory } from '@storybook/react'
+
+import TestIcon from '../../../assets/svgs/brand-element.svg'
+
+import CategoryCard from './CategoryCard'
+
+export default {
+  title: 'Patterns/Cards/CategoryCard',
+  component: CategoryCard,
+  argTypes: {
+    size: {
+      name: 'size',
+      description: 'Select display size',
+      control: {
+        type: 'radio'
+      },
+      options: ['mobile', 'tablet', 'desktop'],
+      defaultValue: 'desktop'
+    },
+    title: {
+      name: 'title',
+      description: 'Title',
+      control: {
+        type: 'text'
+      },
+      defaultValue: 'Room'
+    },
+    alt: {
+      name: 'alt',
+      description: 'Alt text',
+      control: {
+        type: 'text'
+      },
+      defaultValue: 'CategoryCTA'
+    }
+  }
+} as ComponentMeta<typeof CategoryCard>
+
+const Template: ComponentStory<typeof CategoryCard> = args => {
+  const { size } = args
+
+  const setViewportWidth = () => {
+    if (size === 'desktop') {
+      return 'w-[343px]'
+    }
+    if (size === 'tablet') {
+      return 'w-[360px]'
+    }
+    if (size === 'mobile') {
+      return 'w-[308px]'
+    }
+  }
+
+  return (
+    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+    <div className={`${setViewportWidth()} p-4`}>
+      <CategoryCard {...args} />
+    </div>
+  )
+}
+
+export const base = Template.bind({})
+
+base.args = {
+  title: 'Room',
+  href: '/',
+  children: <TestIcon />,
+  icon: <BeakerIcon className="h-4 w-4" />
+}
---
+++ ui/patterns/CatergoyCard/CategoryCard.tsx
@@ -0,0 +1,44 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import type { ReactElement, ReactNode } from 'react'
+import React from 'react'
+
+// import { useBreakpoints } from '@/lib/hooks/useBreakpoints'
+import { Button } from '@/ui/components'
+
+import image from '../../../assets/images/testimage.png'
+// import brandSVGIcon from '../../../assets/svgs/brand-element.svg'
+
+interface CategoryCardProps {
+  href: string
+  alt: string
+  title: string
+  description: string
+  size?: 'desktop' | 'tablet' | 'mobile'
+  children: ReactNode
+  icon?: ReactElement
+}
+
+const CategoryCard = ({ title, href, alt, children, description }: CategoryCardProps) => {
+  //   const { breakpointMd } = useBreakpoints()
+
+  return (
+    <Link href={href}>
+      <a className="group">
+        <div>
+          <span className="relative block h-full w-full transition group-hover:border-primary-base">
+            <Image alt={alt} className="object-cover" width={50} height={50} src={image} />
+          </span>
+          <div className="flex items-center gap-sm">
+            <span className="h-md w-md">{children}</span>
+            <span className="w-full font-bold transition group-hover:border-primary-base">{title}</span>
+            <Button variant="ghost">text</Button>
+          </div>
+          <span className="w-full font-bold transition group-hover:border-primary-base">{description}</span>
+        </div>
+      </a>
+    </Link>
+  )
+}
+
+export default CategoryCard
